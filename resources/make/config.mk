0da6078193c6d23ec76cdc3810eb3ef60dfed188
Marc Siebeneicher <msiebeneicher@users.noreply.github.com>
2020-01-09 15:13:54 +0100

    Added .DEFAULT_GOAL setting (#1)
    Added .DEFAULT_GOAL to enable help as default target

--- resources/make/config.mk
+++ resources/make/config.mk
@@ -29,3 +29,10 @@ MAKEFLAGS += --always-make
 #
 # https://www.gnu.org/software/make/manual/html_node/Special-Targets.html
 #.EXPORT_ALL_VARIABLES:
+
+# .DEFAULT_GOAL
+# Sets the default goal to be used if no targets were specified on the command line
+# (see Arguments to Specify the Goals). The .DEFAULT_GOAL variable allows you to discover 
+# the current default goal, restart the default goal selection algorithm by clearing 
+# its value, or to explicitly set the default goal.
+#.DEFAULT_GOAL := help
